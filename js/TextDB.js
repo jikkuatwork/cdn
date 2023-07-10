8123cd4af718f7151ba571a243da1bab2a350792
Jikku Jose <jikku@storybrain.com>
2023-07-10 12:19:11 +0530

    Update TextDB

--- js/TextDB.js
+++ js/TextDB.js
@@ -13,6 +13,35 @@ export class TextDB {
 
   static randomKey = () => new TextDB().randomKey()
 
+  static getReadOnlyKey = async key => new TextDB(key).getReadOnlyKey()
+
+  static getReadOnlyLink = async key => new TextDB(key).getReadOnlyLink()
+
+  getReadOnlyLink = async () =>
+    `${TextDB.API_URL}/${await this.getReadOnlyKey()}`
+
+  getReadOnlyKey = async () => {
+    const PAGE_URL = "https://textdb.dev/data"
+
+    const extractReadOnlyKey = html => {
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(html, "text/html")
+      const linkElement = doc.querySelector(".data-api a")
+      if (linkElement) {
+        const href = linkElement.href
+        const parts = href.split("/")
+        return parts[parts.length - 1]
+      }
+      return null
+    }
+
+    return await fetch(`${PAGE_URL}/${this.key}`, {
+      method: "GET",
+    })
+      .then(r => r.text())
+      .then(t => extractReadOnlyKey(t))
+  }
+
   read = async () => {
     return await fetch(`${TextDB.API_URL}/${this.key}`)
       .then(r => r.text())
