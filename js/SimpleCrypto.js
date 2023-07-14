b43c1d7fb1f3574220d580a17959ae5ef34a05d3
Jikku Jose <jikku@storybrain.com>
2023-07-14 19:29:05 +0530

    Improve error handling

--- js/SimpleCrypto.js
+++ js/SimpleCrypto.js
@@ -35,14 +35,14 @@ export class SimpleCrypto {
       let cryptoKey = await window.crypto.subtle.importKey(
         "raw",
         hashedKey,
-        "AES-GCM",
+        { name: "AES-GCM", length: 256 },
         false,
         ["encrypt"]
       )
       let iv = window.crypto.getRandomValues(new Uint8Array(12))
       let data = new TextEncoder().encode(message)
       let encrypted = await window.crypto.subtle.encrypt(
-        { name: "AES-GCM", iv: iv },
+        { name: "AES-GCM", iv: iv, tagLength: 128 },
         cryptoKey,
         data
       )
@@ -56,53 +56,62 @@ export class SimpleCrypto {
     } else {
       // Node.js
       const crypto = require("crypto")
-      let iv = crypto.randomBytes(16)
-      let cipher = crypto.createCipheriv("aes-256-cbc", hashedKey, iv)
+      let iv = crypto.randomBytes(12)
+      let cipher = crypto.createCipheriv("aes-256-gcm", hashedKey, iv)
       let encrypted = Buffer.concat([
         cipher.update(message, "utf8"),
         cipher.final(),
       ])
-      return iv.toString("hex") + encrypted.toString("hex")
+      let tag = cipher.getAuthTag()
+      return Buffer.concat([iv, encrypted, tag]).toString("hex")
     }
   }
 
   static async decrypt(encryptedMessage, key) {
-    let hashedKey = await this.getHashedKey(key)
-    if (
-      typeof window !== "undefined" &&
-      window.crypto &&
-      window.crypto.subtle
-    ) {
-      // Browser
-      let cryptoKey = await window.crypto.subtle.importKey(
-        "raw",
-        hashedKey,
-        "AES-GCM",
-        false,
-        ["decrypt"]
-      )
-      let bytes = new Uint8Array(
-        encryptedMessage.match(/.{1,2}/g).map(byte => parseInt(byte, 16))
-      )
-      let iv = bytes.slice(0, 12)
-      let encrypted = bytes.slice(12)
-      let decrypted = await window.crypto.subtle.decrypt(
-        { name: "AES-GCM", iv: iv },
-        cryptoKey,
-        encrypted
-      )
-      return new TextDecoder().decode(decrypted)
-    } else {
-      // Node.js
-      const crypto = require("crypto")
-      let iv = Buffer.from(encryptedMessage.slice(0, 32), "hex")
-      let encryptedText = Buffer.from(encryptedMessage.slice(32), "hex")
-      let decipher = crypto.createDecipheriv("aes-256-cbc", hashedKey, iv)
-      let decrypted = Buffer.concat([
-        decipher.update(encryptedText),
-        decipher.final(),
-      ])
-      return decrypted.toString("utf8")
+    try {
+      let hashedKey = await this.getHashedKey(key)
+      if (
+        typeof window !== "undefined" &&
+        window.crypto &&
+        window.crypto.subtle
+      ) {
+        // Browser
+        let cryptoKey = await window.crypto.subtle.importKey(
+          "raw",
+          hashedKey,
+          { name: "AES-GCM", length: 256 },
+          false,
+          ["decrypt"]
+        )
+        let bytes = new Uint8Array(
+          encryptedMessage.match(/.{1,2}/g).map(byte => parseInt(byte, 16))
+        )
+        let iv = bytes.slice(0, 12)
+        let encrypted = bytes.slice(12)
+        let decrypted = await window.crypto.subtle.decrypt(
+          { name: "AES-GCM", iv: iv },
+          cryptoKey,
+          encrypted
+        )
+        return new TextDecoder().decode(decrypted)
+      } else {
+        // Node.js
+        const crypto = require("crypto")
+        let data = Buffer.from(encryptedMessage, "hex")
+        let iv = data.slice(0, 12)
+        let encryptedText = data.slice(12, data.length - 16)
+        let tag = data.slice(data.length - 16)
+        let decipher = crypto.createDecipheriv("aes-256-gcm", hashedKey, iv)
+        decipher.setAuthTag(tag)
+        let decrypted = Buffer.concat([
+          decipher.update(encryptedText),
+          decipher.final(),
+        ])
+        return decrypted.toString("utf8")
+      }
+    } catch (error) {
+      // Decryption failed, return null
+      return null
     }
   }
 }
